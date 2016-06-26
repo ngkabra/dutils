a4366ff90f56468df493ed344614d773d3d8904e
Navin Kabra <navin@smriti.com>
2016-06-26 09:46:41 +0530

    Fixes for django1.9 syncdb removal

--- replacedb.py
+++ replacedb.py
@@ -8,18 +8,20 @@ We do not load django because the database might not be uptodate.
 Drop the database and recreate it so db is clean.
 from being sent.
 '''
-
-import logging
-logger = logging.getLogger(__name__)
-
 import argparse
 from datetime import datetime
 import os
-from os.path import dirname, exists, expanduser, join
+from os.path import exists, expanduser
 import sys
 import subprocess
 import MySQLdb as mysql
+from logging.config import fileConfig
+import django
+from django.conf import settings
+from django.core.management import ManagementUtility
 
+import logging
+logger = logging.getLogger(__name__)
 
 
 def replace_db(dbfile):
@@ -39,7 +41,6 @@ def replace_db(dbfile):
     if db['NAME'] in ('navin_rs', 'navin_rsph'):
         raise Exception('WTF? Are you trying to replace production?')
 
-
     # drop and recreate database
     logger.debug('mysql connect {}'.format(db['NAME']))
     rootdb = mysql.connect(user=db['NAME'],
@@ -50,25 +51,24 @@ def replace_db(dbfile):
         logger.debug('Dropped database {}'.format(db['NAME']))
     except:
         pass                              # ignore drop database errors
-    c.execute('create database %s character set utf8 collate utf8_general_ci' % db['NAME'])
+    c.execute('create database {} character set utf8 collate utf8_general_ci'.format(db['NAME']))
     logger.debug('Created database {}'.format(db['NAME']))
     c.close()
     rootdb.close()
-
 
     logger.info('replacedb started at {0:%H:%M:%S}'.format(datetime.now()))
     # now load the data. For that first gunzip it,
     # then run mysql in a subprocess
     unzipproc = subprocess.Popen(['gunzip', '--stdout', dbfile],
                                  stdout=subprocess.PIPE)
-    mysqlproc = subprocess.Popen(['mysql', '-u', db['USER'],
-                                  '--password=%s' % (db['PASSWORD'],),
-                                  db['NAME']],
-                                 stdin=unzipproc.stdout, stdout=subprocess.PIPE)
+    mysqlproc = subprocess.Popen(
+        ['mysql', '-u', db['USER'],
+         '--password=%s' % (db['PASSWORD'],),
+         db['NAME']],
+        stdin=unzipproc.stdout, stdout=subprocess.PIPE)
     unzipproc.stdout.close()
     logger.debug('Waiting for gunzip|mysql process')
     mysqlproc.wait()
-
 
     # notifications_TODO: truncate the notifications
     # mydb = mysql.connect(user=db['USER'],
@@ -83,7 +83,7 @@ parser.add_argument('-d', '--debug', action='store_true',
                     help='log debug messages to stderr')
 parser.add_argument('-p', '--project-path', nargs='*',
                     help='directories to add to project path')
-parser.add_argument('-s', '--settings-module', 
+parser.add_argument('-s', '--settings-module',
                     default='settings',
                     help='settings for DJANGO_SETTINGS_MODULE')
 parser.add_argument('-v', '--verbose', action='store_true',
@@ -97,8 +97,6 @@ parser.add_argument('-n', '--no-syncdb',
 parser.add_argument('file', help='Dump of database')
 
 args = parser.parse_args()
-from os.path import expanduser
-from logging.config import fileConfig
 handlers = 'console,file' if args.verbose else 'file'
 fileConfig(expanduser('~/.pylog.cfg'),
            defaults=dict(
@@ -117,13 +115,14 @@ for path_el in args.project_path[::-1]:
     sys.path.insert(0, path_el)  # insert in reverse order to get correct order
 if args.settings_module:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', args.settings_module)
-from django.conf import settings
-from django.core.management import ManagementUtility
 
 replace_db(args.file)
 
 if args.no_syncdb:
     cmds = []
+elif django.VERSION[0] > 1 or django.VERSION[1] >= 9:
+    # no syncdb in django 1.9 or above
+    cmds = [['migrate']]
 else:
     cmds = [['syncdb'], ['migrate']]
 
