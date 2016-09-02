29da35039a667335eae4a8c3ca3c0fee080872c8
Navin Kabra <navin@smriti.com>
2016-09-02 14:44:02 +0530

    Allow use of SendGrid as email service

--- send_mail.py
+++ send_mail.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from urllib2 import HTTPError
 
 from django.conf import settings
 
@@ -7,6 +8,7 @@ try:
     import sendgrid
 except ImportError:
     sendgrid = None
+
 
 
 class MailError(Exception):
@@ -17,19 +19,44 @@ def sendgrid_send(subject,
                   message,
                   from_email,
                   to_emails,
-                  fail_silently=False):
-    sg = sendgrid.SendGridClient(settings.SENDGRID_KEY, raise_errors=True)
-    msg = sendgrid.Mail()
-    msg.add_to(to_emails)
-    msg.set_subject(subject)
-    msg.set_from(from_email)
-    msg.set_text(message)
+                  from_email_name="RSphinx Admin",
+                  cc_emails=[],
+                  bcc_emails=[]):
+
+    sg = sendgrid.SendGridAPIClient(apikey=settings.SENDGRID_KEY)
+    data = {
+        "personalizations": [
+            {
+                "to": [{"email": e} for e in to_emails],
+                "subject": subject,
+            }
+        ],
+        "from": {
+            "email": from_email,
+            "name": from_email_name
+        },
+        "content": [
+            {
+                "type": "text/plain",
+                "value": message
+            }
+        ]
+    }
+
+    if cc_emails:
+        data['personalizations'][0]['cc'] = [{'email': e} for e in cc_emails]
+
+    if bcc_emails:
+        data['personalizations'][0]['bcc'] = [{'email': e} for e in bcc_emails]
 
     try:
-        status, msg = sg.send(msg)
-    except sendgrid.SendGridError:
-        if not fail_silently:
-            raise MailError('Error sending mail')
+        response = sg.client.mail.send.post(request_body=data)
+    except HTTPError as e:
+        raise MailError('HTTPError: {}'.format(e.read()))
+
+    if response.status_code % 100 != 2:  # not 2xx
+        raise MailError("status={}, body={}".format(
+            response.status_code, response.body))
 
 
 def mandrill_send(subject,
