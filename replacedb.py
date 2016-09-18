86bd1eb13df7641b86cf7ad19f23601f8fc6a721
Navin Kabra <navin@smriti.com>
2016-09-18 21:29:48 +0530

    Upgrade to django17

--- replacedb.py
+++ replacedb.py
@@ -8,17 +8,15 @@ We do not load django because the database might not be uptodate.
 Drop the database and recreate it so db is clean.
 from being sent.
 '''
-
-import logging
-logger = logging.getLogger(__name__)
-
 import argparse
 import os
-from os.path import dirname, exists, expanduser, join
+from os.path import exists, expanduser
 import sys
 import subprocess
 import MySQLdb as mysql
 
+import logging
+logger = logging.getLogger(__name__)
 
 
 def replace_db(dbfile):
@@ -38,7 +36,6 @@ def replace_db(dbfile):
     if db['NAME'] in ('navin_rs', 'navin_rsph'):
         raise Exception('WTF? Are you trying to replace production?')
 
-
     # drop and recreate database
     logger.debug('mysql connect {}'.format(db['NAME']))
     rootdb = mysql.connect(user=db['NAME'],
@@ -49,24 +46,24 @@ def replace_db(dbfile):
         logger.debug('Dropped database {}'.format(db['NAME']))
     except:
         pass                              # ignore drop database errors
-    c.execute('create database %s character set utf8 collate utf8_general_ci' % db['NAME'])
+    c.execute('create database {} character set utf8 '
+              'collate utf8_general_ci'.format(db['NAME']))
     logger.debug('Created database {}'.format(db['NAME']))
     c.close()
     rootdb.close()
-
 
     # now load the data. For that first gunzip it,
     # then run mysql in a subprocess
     unzipproc = subprocess.Popen(['gunzip', '--stdout', dbfile],
                                  stdout=subprocess.PIPE)
-    mysqlproc = subprocess.Popen(['mysql', '-u', db['USER'],
-                                  '--password=%s' % (db['PASSWORD'],),
-                                  db['NAME']],
-                                 stdin=unzipproc.stdout, stdout=subprocess.PIPE)
+    mysqlproc = subprocess.Popen(
+        ['mysql', '-u', db['USER'],
+         '--password=%s' % (db['PASSWORD'],),
+         db['NAME']],
+        stdin=unzipproc.stdout, stdout=subprocess.PIPE)
     unzipproc.stdout.close()
     logger.debug('Waiting for gunzip|mysql process')
     mysqlproc.wait()
-
 
     # notifications_TODO: truncate the notifications
     # mydb = mysql.connect(user=db['USER'],
@@ -81,7 +78,7 @@ parser.add_argument('-d', '--debug', action='store_true',
                     help='log debug messages to stderr')
 parser.add_argument('-p', '--project-path', nargs='*',
                     help='directories to add to project path')
-parser.add_argument('-s', '--settings-module', 
+parser.add_argument('-s', '--settings-module',
                     default='settings',
                     help='settings for DJANGO_SETTINGS_MODULE')
 parser.add_argument('-v', '--verbose', action='store_true',
@@ -116,7 +113,7 @@ for path_el in args.project_path[::-1]:
 if args.settings_module:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', args.settings_module)
 from django.conf import settings
-from django.core.management import ManagementUtility
+from django.core.management import execute_from_command_line
 
 replace_db(args.file)
 
@@ -128,5 +125,6 @@ else:
 if args.demo:
     cmds.append(['runcmd', args.fixdemoscript])
 for cmd in cmds:
-    utility = ManagementUtility([sys.argv[0]] + cmd)
-    utility.execute()
+    execute_from_command_line([sys.argv[0]] + cmd)
+    # utility = ManagementUtility([sys.argv[0]] + cmd)
+    # utility.execute()
