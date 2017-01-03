4d2eb7659ee887684003c56e2b07b87eb59db34d
Navin Kabra <navin@smriti.com>
2017-01-04 04:53:39 +0530

    Fixes for django1.9 in shell+

--- management/commands/shell+.py
+++ management/commands/shell+.py
@@ -2,15 +2,22 @@
 # Don't want django-extensions just for shell_plus
 from django.core.management.base import NoArgsCommand
 
+
 class Command(NoArgsCommand):
-    help = "Runs a Python interactive interpreter. Tries to use IPython, if it's available."
+    help = ("Runs a Python interactive interpreter. "
+            "Tries to use IPython, if it's available.")
+
     def ipython(self, imported_objects):
         from IPython import embed
         embed(user_ns=imported_objects)
 
     def handle_noargs(self, **options):
         imported_objects = {}
-        from django.db.models.loading import get_models
+        try:
+            from django.db.models.loading import get_models
+        except ImportError:
+            from django.apps import apps
+            get_models = apps.get_models
         for m in get_models():
             imported_objects[m.__name__] = m
 
@@ -18,14 +25,15 @@ class Command(NoArgsCommand):
             self.ipython(imported_objects)
         except ImportError:
             import code
-            try: # Try activating rlcompleter, because it's handy.
+            try:  # Try activating rlcompleter, because it's handy.
                 import readline
             except ImportError:
                 pass
             else:
-                # We don't have to wrap the following import in a 'try', because
+                # We don't have to wrap the following import in a 'try'
                 # we already know 'readline' was imported successfully.
                 import rlcompleter
-                readline.set_completer(rlcompleter.Completer(imported_objects).complete)
+                readline.set_completer(
+                    rlcompleter.Completer(imported_objects).complete)
                 readline.parse_and_bind("tab:complete")
             code.interact(local=imported_objects)
