cd81bf69172a4ab281181a586476da76e3664e66
Navin Kabra <navin@smriti.com>
2018-04-05 13:36:53 +0530

    Bug fix for management command argument processing

--- management/commands/runcmd.py
+++ management/commands/runcmd.py
@@ -3,8 +3,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('script')
-        parser.add_argument('args', nargs='*')
+        parser.add_argument('arguments', nargs='*')
 
     def handle(self, *args, **options):
-        mod = __import__(options['script'], [], [], [' '])
-        mod.run(*options.get('args', []))
+        script = args[0]
+        arguments = args[1:]
+        mod = __import__(script, [], [], [' '])
+        mod.run(*arguments)
