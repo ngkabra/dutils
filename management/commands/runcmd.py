7c258444222a156d380789b424a439f52ab4ff30
Navin Kabra <navin@smriti.com>
2020-11-12 18:47:18 +0530

    Add comment to runcmd explaining what it does and why

--- management/commands/runcmd.py
+++ management/commands/runcmd.py
@@ -1,6 +1,14 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
+    '''Run a script with django initialized
+
+    Import a standalone script and call `run` on it
+    This is not to run a managepy django command
+    Django will be initialized
+
+    Expects arguments as a0 a1 kw1=kwarg1 kw2=kwarg2
+    '''
     def add_arguments(self, parser):
         parser.add_argument('script')
         parser.add_argument('arguments', nargs='*')
